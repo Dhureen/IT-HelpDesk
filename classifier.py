ea1d02505af76e58e964b3d770be2ba26f2322b3
Dhureen <Dhureen@users.noreply.github.com>
2016-11-07 17:18:35 +0530

    Add files via upload

--- classifier.py
+++ classifier.py
@@ -19,12 +19,14 @@ def preprocess(sentence):
     stpwords = set(stopwords.words('english'))
     stopremoved = [w.lower() for w in words if w.lower() not in stpwords]
     numremoved = [w for w in stopremoved if not any(c.isdigit() for c in w)]
-    return numremoved
+    punctremoved = [w for w in numremoved if re.match(r"^([\W]+|req|nonitmgmt)$",w) is None]
+        
+    return punctremoved
 
 def find_features(sentence):
     sent_features = {}
     for w in word_features:
-        sent_features[w] = (w in sentence)
+        sent_features[w[0]] = (w[0] in sentence)
         
     return sent_features
 
@@ -33,7 +35,11 @@ for w in dataset:
     allWords += preprocess(w[0])
     
 dist = nltk.FreqDist(allWords)
-word_features = list(dist.keys())[:50]
+#word_features = list(dist.keys())
+word_features = sorted(dist.iteritems(), key=operator.itemgetter(1))
+word_features = word_features[len(word_features) - 100:]
+print word_features
+#print sorted_x[len(sorted_x) - 50:]
 
 featuresets = [(find_features(w[0]), w[1]) for w in dataset]
 random.shuffle(featuresets)
@@ -43,7 +49,7 @@ testing_set = featuresets[len(featuresets)/2:]
 
 classifier = nltk.NaiveBayesClassifier.train(training_set)
 print("Classifier accuracy percent:",(nltk.classify.accuracy(classifier, testing_set))*100)
-classifier.show_most_informative_features(15)
+classifier.show_most_informative_features(25)
 
 class_f = open('naivebayes.pickle','wb')
 pickle.dump(classifier,class_f)
@@ -53,6 +59,5 @@ class_f.close()
 #for e in values:
  #       print str(e)
 
-#sorted_x = sorted(dist.iteritems(), key=operator.itemgetter(1))
-#print sorted_x
+
 
